536c16bfa2eb84b12a640649de9caa3e03e39f35
daniildmytriiev <daniildtd@gmail.com>
2024-03-21 11:56:52 +0200

    HW6 task 3 PEP8

--- HomeWork/6/HW6_3.py
+++ HomeWork/6/HW6_3.py
@@ -1,26 +1,29 @@
 def get_user_input():
-  return eval(input("Enter a list, tuple, dictionary, or integer: "))
+    """Gets user input and returns it in its original format."""
+    return eval(input("Enter a list, tuple, dictionary, or integer: "))
 def print_data_type(data):
-  try:
-    if isinstance(data, dict):
-      print(f"User is going to work with a dictionary.")
-    else:
-      raise TypeError
-  except TypeError:
+    """Attempts to convert input to different data types and prints a message."""
     try:
-      if isinstance(data, list):
-        print(f"User is going to work with a list.")
-      else:
-        raise TypeError
-
-    except TypeError:
-      try:
-        if isinstance(data, tuple):
-          print(f"User is going to work with a tuple.")
+        if isinstance(data, dict):
+            print(f"User is going to work with a dictionary.")
         else:
-          raise TypeError
+            raise TypeError
+    except TypeError:
+        try:
+            if isinstance(data, list):
+                print(f"User is going to work with a list.")
+            else:
+                raise TypeError
 
-      except TypeError:
-        print(f"User is going to work with an integer.")
+        except TypeError:
+            try:
+                if isinstance(data, tuple):
+                    print(f"User is going to work with a tuple.")
+                else:
+                    raise TypeError
+
+            except TypeError:
+                print(f"User is going to work with an integer.")
+# Example usage
 user_data = get_user_input()
 print_data_type(user_data)
