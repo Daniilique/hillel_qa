1934c9d27dabed48dbe2e871450e0b13234ed8a2
daniildmytriiev <daniildtd@gmail.com>
2024-04-29 09:43:16 +0300

    HW11 try 2 - OOP

--- HomeWork/11/HW11_1.py
+++ HomeWork/11/HW11_1.py
@@ -1,14 +1,48 @@
 class Game:
-    # Class attribute (avg time to finish a game)
+    # Class attribute
     default_time_to_finish = 10.0  # In hours
-
     def __init__(self, name, time_to_finish=None):
-        self.name = name
-        self.time_to_finish = time_to_finish if time_to_finish is not None else self.default_time_to_finish
+        self._name = name
+        self._time_to_finish = time_to_finish if time_to_finish is not None else self.default_time_to_finish
+
+    # Getter and setter for instance attribute 'name'
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    # Getter and setter for instance attribute 'time_to_finish'
+    def get_time_to_finish(self):
+        return self._time_to_finish
+
+    def set_time_to_finish(self, time_to_finish):
+        self._time_to_finish = time_to_finish
+
+    # Getter and setter for class attribute 'default_time_to_finish'
+    @classmethod
+    def get_default_time_to_finish(cls):
+        return cls.default_time_to_finish
+
+    @classmethod
+    def set_default_time_to_finish(cls, default_time_to_finish):
+        cls.default_time_to_finish = default_time_to_finish
 
 
+# Example usage
 game1 = Game("Tetris")
 game2 = Game("Mario", 15.5)
 
-print(f"Game 1: {game1.name}, Time to Finish: {game1.time_to_finish} hours")
-print(f"Game 2: {game2.name}, Time to Finish: {game2.time_to_finish} hours")
+print(f"Game 1: {game1.get_name()}, Time to Finish: {game1.get_time_to_finish()} hours")
+print(f"Game 2: {game2.get_name()}, Time to Finish: {game2.get_time_to_finish()} hours")
+
+# Changing instance attributes using setters
+game1.set_name("Pac-Man")
+game1.set_time_to_finish(8.0)
+
+print(f"Updated Game 1: {game1.get_name()}, Time to Finish: {game1.get_time_to_finish()} hours")
+
+# Changing class attribute using setter
+Game.set_default_time_to_finish(12.0)
+
+print(f"Default Time to Finish: {Game.get_default_time_to_finish()} hours")
--- HomeWork/11/HW11_2.py
+++ HomeWork/11/HW11_2.py
@@ -1,5 +1,20 @@
 class Game:
-    pass
+    def __init__(self, name, time_to_finish=None):
+        self._name = name
+        self._time_to_finish = time_to_finish
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_time_to_finish(self):
+        return self._time_to_finish
+
+    def set_time_to_finish(self, time_to_finish):
+        self._time_to_finish = time_to_finish
+
 
 class ExtendedGame(Game):
     def __init__(self, name, time_to_finish=None, multiplayer=False):
@@ -9,5 +24,17 @@ class ExtendedGame(Game):
     def is_multiplayer(self):
         return self.multiplayer
 
+    # New method added to the child class
+    def time_per_player(self, num_players):
+        if self.multiplayer:
+            return self._time_to_finish / num_players
+        else:
+            return self._time_to_finish
+
+
+# Example usage
 extended_game = ExtendedGame("Call of Duty", 20.0, True)
-print(f"Extended Game: {extended_game.name}, Time to Finish: {extended_game.time_to_finish} hours, Multiplayer: {extended_game.is_multiplayer()}")
+print(f"Extended Game: {extended_game.get_name()}, Time to Finish: {extended_game.get_time_to_finish()} hours, Multiplayer: {extended_game.is_multiplayer()}")
+
+# Using the new method
+print(f"Time per player: {extended_game.time_per_player(4)} hours")
